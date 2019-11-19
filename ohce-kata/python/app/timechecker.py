c6b73d0087c91ef562106ed7f22a3fc987349661
Lorens León <lorensleon@gmail.com>
2019-11-19 09:53:42 +0100

    refactor first test

--- ohce-kata/python/app/timechecker.py
+++ ohce-kata/python/app/timechecker.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+SIX_AM = 6
+TWELVE_PM = 12
 
-def is_morning(date: datetime):
-    return date.hour >= 6 and date.hour < 12
+
+def is_morning(date: datetime) -> bool:
+    return SIX_AM <= date.hour < TWELVE_PM
--- ohce-kata/python/tests/test_timechecker_is_morning.py
+++ ohce-kata/python/tests/test_timechecker_is_morning.py
@@ -4,6 +4,10 @@ from app.timechecker import is_morning
 
 
 def test_is_morning_should_return_true_when_the_time_is_between_06_00_and_12_00():
-    date = datetime(year=2019, month=1, day=1, hour=7)
+    date = _get_date_at_hour(7)
 
     assert is_morning(date) is True
+
+
+def _get_date_at_hour(hour: int) -> datetime:
+    return datetime(year=2019, month=1, day=1, hour=hour)
