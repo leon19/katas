a0e7ac2a2121bf32ea026605fee6cd661f4ab8a0
Lorens León <lorensleon@gmail.com>
2019-11-19 10:13:22 +0100

    add more tests

--- ohce-kata/python/tests/test_timechecker_is_morning.py
+++ ohce-kata/python/tests/test_timechecker_is_morning.py
@@ -9,5 +9,29 @@ def test_is_morning_should_return_true_when_the_time_is_between_06_00_and_12_00(
     assert is_morning(date) is True
 
 
-def _get_date_at_hour(hour: int) -> datetime:
-    return datetime(year=2019, month=1, day=1, hour=hour)
+def test_is_morning_should_return_true_when_the_time_is_exactly_06_00():
+    date = _get_date_at_hour(6)
+
+    assert is_morning(date) is True
+
+
+def test_is_morning_should_return_false_when_the_time_is_exactly_12_00():
+    date = _get_date_at_hour(12)
+
+    assert is_morning(date) is False
+
+
+def test_is_morning_should_return_false_when_the_time_is_lower_than_06_00():
+    date = _get_date_at_hour(5, minute=59)
+
+    assert is_morning(date) is False
+
+
+def test_is_morning_should_return_false_when_the_time_is_higher_than_12_00():
+    date = _get_date_at_hour(13)
+
+    assert is_morning(date) is False
+
+
+def _get_date_at_hour(hour: int, minute: int = 0) -> datetime:
+    return datetime(year=2019, month=1, day=1, hour=hour, minute=minute)
