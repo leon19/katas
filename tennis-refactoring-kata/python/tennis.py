a6e9ed1f7f63f8411c6b8221deafdacb4a5af6bf
Lorens León <lorensleon@gmail.com>
2019-12-16 15:31:17 +0100

    chore: early return

--- tennis-refactoring-kata/python/tennis.py
+++ tennis-refactoring-kata/python/tennis.py
@@ -12,28 +12,32 @@ class TennisGame1:
             self.player_two_points += 1
 
     def score(self):
-        result = ""
-        temp_score = 0
         if self.player_one_points == self.player_two_points:
-            result = {0: "Love-All", 1: "Fifteen-All", 2: "Thirty-All",}.get(self.player_one_points, "Deuce")
-        elif self.player_one_points >= 4 or self.player_two_points >= 4:
+            return {0: "Love-All", 1: "Fifteen-All", 2: "Thirty-All"}.get(self.player_one_points, "Deuce")
+
+        if self.player_one_points >= 4 or self.player_two_points >= 4:
             minus_result = self.player_one_points - self.player_two_points
+
             if minus_result == 1:
-                result = "Advantage " + self.player_one_name
-            elif minus_result == -1:
-                result = "Advantage " + self.player_two_name
-            elif minus_result >= 2:
-                result = "Win for " + self.player_one_name
+                return "Advantage " + self.player_one_name
+
+            if minus_result == -1:
+                return "Advantage " + self.player_two_name
+
+            if minus_result >= 2:
+                return "Win for " + self.player_one_name
+
+            return "Win for " + self.player_two_name
+
+        result = ""
+        for i in range(1, 3):
+            if i == 1:
+                temp_score = self.player_one_points
             else:
-                result = "Win for " + self.player_two_name
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.player_one_points
-                else:
-                    result += "-"
-                    temp_score = self.player_two_points
-                result += {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty",}[temp_score]
+                result += "-"
+                temp_score = self.player_two_points
+            result += {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}[temp_score]
+
         return result
 
 
